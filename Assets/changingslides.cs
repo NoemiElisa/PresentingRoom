db936450e52427746d887fa4c45d7cf48ff9f422
Noemi Elisa <noemiElis@gmail.com>
2018-09-06 01:09:30 +0100

    more assets

---
+++ Assets/changingslides.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changingslides : MonoBehaviour {
+
+    public List<Texture> slides;
+    public int slideSelected;
+    public RawImage raw;
+    private Time timeEntered;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeSlide()
+    {
+        if (slides != null && slides.Count >0)
+        {
+            slideSelected++;
+            if (slideSelected >= slides.Count)
+            {
+                slideSelected = 0;
+            }
+            raw.texture = slides[slideSelected];
+
+        }
+
+    }
+
+    // count the time on enter
+    public void CountingEntered()
+    {
+       // timeEntered = Time.captureFramerate(); 
+    }
+
+    // reset to 0 on exit
+
+
+}
